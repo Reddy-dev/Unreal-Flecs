3b0f8f7a1eeed36acff9e574ecc1a87a4f51b207
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-02-23 10:12:02 -0500

    Update FlecsComponentHandle.h

--- Source/UnrealFlecs/Entities/FlecsComponentHandle.h
+++ Source/UnrealFlecs/Entities/FlecsComponentHandle.h
@@ -7,13 +7,49 @@
 #include "FlecsComponentHandle.generated.h"
 
 /**
- * @TODO: Equivalent to flecs::untyped_component and flecs::component<T>
+ * Equivalent to flecs::untyped_component and flecs::component<T>
  */
 USTRUCT(BlueprintType, meta = (DisableSplitPin))
-struct alignas(8) UNREALFLECS_API FFlecsComponentHandle : public FFlecsEntityHandle
+struct UNREALFLECS_API FFlecsComponentHandle : public FFlecsEntityHandle
 {
 	GENERATED_BODY()
 
 public:
+	FFlecsComponentHandle() = default;
+	
+	FFlecsComponentHandle(const FFlecsEntityHandle& InEntityHandle)
+		: FFlecsEntityHandle(InEntityHandle)
+	{
+		solid_checkf(IsComponent(), TEXT("Entity is not a component"));
+	}
 
+	FORCEINLINE NO_DISCARD flecs::untyped_component GetUntypedComponent() const
+	{
+		return GetUntypedComponent_Unsafe();
+	}
+
+	template <typename T>
+	FORCEINLINE NO_DISCARD flecs::component<T> GetTypedComponent() const
+	{
+		return flecs::component<T>(GetFlecsWorld_Internal(), GetEntity());
+	}
+	
+
+	FORCEINLINE operator flecs::untyped_component() const
+	{
+		return GetUntypedComponent();
+	}
+
+	template <typename T>
+	FORCEINLINE operator flecs::component<T>() const
+	{
+		return GetTypedComponent<T>();
+	}
+
+	template <typename T>
+	FORCEINLINE void AddBit(const FString& InName, const T InValue) const
+	{
+		GetUntypedComponent_Unsafe().bit<T>(StringCast<char>(*InName).Get(), InValue);
+	}
+	
 }; // struct FFlecsComponentHandle
