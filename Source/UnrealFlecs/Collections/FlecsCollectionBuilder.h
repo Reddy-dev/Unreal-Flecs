fb07b0418526370da46fc44972ccc22675ef3a60
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-04-27 01:42:33 -0400

    Create FlecsCollectionBuilder.h

---
+++ Source/UnrealFlecs/Collections/FlecsCollectionBuilder.h
@@ -0,0 +1,27 @@
+﻿// Elie Wiese-Namir © 2025. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FlecsCollectionBuilder.generated.h"
+
+USTRUCT(BlueprintType)
+struct UNREALFLECS_API FFlecsCollectionItem
+{
+	GENERATED_BODY()
+
+public:
+	
+}; // struct FFlecsCollectionItem
+
+USTRUCT(BlueprintType)
+struct UNREALFLECS_API FFlecsCollectionBuilder
+{
+	GENERATED_BODY()
+
+public:
+
+private:
+	
+}; // struct FFlecsCollectionBuilder
+
