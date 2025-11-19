7c0645ac0826e90a1e1efb955bc11ad920558c9c
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-11-19 17:36:45 -0500

    Move SolidMacros into UnrealFlecs Repo (will subtree on solid macros github repo)

--- Source/UnrealFlecs/UnrealFlecs.cpp
+++ Source/UnrealFlecs/UnrealFlecs.cpp
@@ -7,9 +7,9 @@
 #include "General/FlecsOSAPI.h"
 #include "Entities/FlecsDefaultEntityEngine.h"
 
-#if !defined(SOLID_MACROS_VERSION) || SOLID_MACROS_VERSION != EXPECTED_SOLID_MACROS_VERSION
+/*#if !defined(SOLID_MACROS_VERSION) || SOLID_MACROS_VERSION != EXPECTED_SOLID_MACROS_VERSION
 #error "The Solid Macros version is not as expected. Please update the Solid Macros to the latest version."
-#endif // #if !defined(SOLID_MACROS_VERSION) || SOLID_MACROS_VERSION != EXPECTED_SOLID_MACROS_VERSION
+#endif // #if !defined(SOLID_MACROS_VERSION) || SOLID_MACROS_VERSION != EXPECTED_SOLID_MACROS_VERSION*/
 
 #define LOCTEXT_NAMESPACE "FUnrealFlecsModule"
 
