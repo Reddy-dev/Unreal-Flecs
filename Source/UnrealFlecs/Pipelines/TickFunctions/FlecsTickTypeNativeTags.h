58abf1e1a39ff651cb2abfdc95e7203d5ebfaefe
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-12-01 22:45:48 -0500

    Tick Native Tags

---
+++ Source/UnrealFlecs/Pipelines/TickFunctions/FlecsTickTypeNativeTags.h
@@ -0,0 +1,17 @@
+﻿// Elie Wiese-Namir © 2025. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "NativeGameplayTags.h"
+
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(FlecsTickType_Root);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(FlecsTickType_PrePhysics);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(FlecsTickType_DuringPhysics);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(FlecsTickType_PostPhysics);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(FlecsTickType_PostUpdateWork);
+
+
+
+
