af7b7dd52468798e3607a5a56bbac7621f26a951
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-12-20 21:06:54 -0500

    Update UnrealFlecsTests.Build.cs

--- Source/UnrealFlecsTests/UnrealFlecsTests.Build.cs
+++ Source/UnrealFlecsTests/UnrealFlecsTests.Build.cs
@@ -25,6 +25,7 @@ public class UnrealFlecsTests : ModuleRules
                 "AutomationUtils",
                 "GameplayTags",
                 "CQTest",
+                "FunctionalTesting",
             }
         );
 
