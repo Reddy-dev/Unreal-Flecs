c9e32d644a46889a5e51b0d2162823009272e6db
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2026-05-29 21:44:01 -0400

    Structs

---
+++ Source/UnrealFlecs/Public/Networking/FlecsNetId.h
@@ -0,0 +1,26 @@
+﻿// Elie Wiese-Namir © 2025. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "SequentialID.h"
+
+#include "FlecsNetId.generated.h"
+
+USTRUCT()
+struct UNREALFLECS_API FFlecsNetId : public FSequentialIDBase
+{
+	GENERATED_BODY()
+	
+public:
+	FORCEINLINE FFlecsNetId() = default;
+	
+	FORCEINLINE explicit FFlecsNetId(const uint32 InID)
+		: FSequentialIDBase(InID)
+	{
+	}
+	
+}; // struct FFlecsNetId
+
+
---
+++ Source/UnrealFlecs/Public/Networking/FlecsNetReplicated.h
@@ -0,0 +1,17 @@
+﻿// Elie Wiese-Namir © 2025. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "FlecsNetReplicated.generated.h"
+
+/**
+ * @brief Generically syncs the owning entities id, used for components and regular entities
+ */
+USTRUCT()
+struct UNREALFLECS_API FFlecsNetReplicated
+{
+	GENERATED_BODY()
+}; // struct FFlecsNetReplicated
+
