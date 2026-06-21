a90103c9755679232ed9375880f11dbcea646559
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2026-06-21 10:49:15 -0400

    Update FlecsTypeRegistryEngineSubsystem.cpp

--- Source/UnrealFlecs/Private/Properties/FlecsTypeRegistryEngineSubsystem.cpp
+++ Source/UnrealFlecs/Private/Properties/FlecsTypeRegistryEngineSubsystem.cpp
@@ -2,6 +2,8 @@
 
 #include "Properties/FlecsTypeRegistryEngineSubsystem.h"
 
+#include "Containers/Queue.h"
+
 #include "Logs/FlecsCategories.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(FlecsTypeRegistryEngineSubsystem)
