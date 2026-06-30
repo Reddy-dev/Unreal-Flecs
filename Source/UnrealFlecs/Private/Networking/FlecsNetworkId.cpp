0c0e5151e68944909be1a3f276e43333c7041623
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2026-06-30 01:55:07 -0400

    NetworkId

---
+++ Source/UnrealFlecs/Private/Networking/FlecsNetworkId.cpp
@@ -0,0 +1,5 @@
+﻿// Elie Wiese-Namir © 2026. All Rights Reserved.
+
+#include "Networking/FlecsNetworkId.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(FlecsNetworkId)
---
+++ Source/UnrealFlecs/Public/Networking/FlecsNetworkId.h
@@ -0,0 +1,21 @@
+﻿// Elie Wiese-Namir © 2026. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "SequentialID.h"
+
+#include "FlecsNetworkId.generated.h"
+
+USTRUCT(BlueprintType)
+struct UNREALFLECS_API FFlecsNetworkId : public FSequentialIDBase
+{
+	GENERATED_BODY()
+	
+public:
+	FORCEINLINE FFlecsNetworkId() = default;
+	FORCEINLINE explicit FFlecsNetworkId(const uint32 InId) : FSequentialIDBase(InId) {}
+}; // struct FFlecsNetworkId
+
+
