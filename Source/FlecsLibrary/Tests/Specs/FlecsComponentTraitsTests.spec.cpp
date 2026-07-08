88f4476079a83d2dfa67d87ce310fdffd771fe78
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2026-07-07 22:08:35 -0400

    Create FlecsComponentTraitsTests.spec.cpp

---
+++ Source/FlecsLibrary/Tests/Specs/FlecsComponentTraitsTests.spec.cpp
@@ -0,0 +1,20 @@
+﻿// Elie Wiese-Namir © 2026. All Rights Reserved.
+
+#include "Misc/AutomationTest.h"
+
+#include "Bake/FlecsTestUtils.h"
+
+#if WITH_AUTOMATION_TESTS && defined(FLECS_TESTS)
+
+BEGIN_DEFINE_SPEC(FFlecsComponentTraitsTestsSpec,
+                  "FlecsLibrary.ComponentTraits",
+                  EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter);
+
+END_DEFINE_SPEC(FFlecsComponentTraitsTestsSpec);
+
+void FFlecsComponentTraitsTestsSpec::Define()
+{
+	
+}
+
+#endif // WITH_AUTOMATION_TESTS && defined(FLECS_TESTS)
