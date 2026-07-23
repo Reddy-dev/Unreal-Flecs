fa862d6c55a65bd049a9a8c16f2afe6450195658
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2026-07-23 15:09:06 -0400

    Update FlecsStageTests.cpp

--- Source/UnrealFlecsTests/Tests/Basic/FlecsStageTests.cpp
+++ Source/UnrealFlecsTests/Tests/Basic/FlecsStageTests.cpp
@@ -22,6 +22,40 @@ protected:
 
 public:
 
+	TEST_METHOD(MainThreadSystemGetStage_IterAPI_ReturnsMainStageObject)
+	{
+		bool bSystemFired = false;
+		bool bIteratorWorldIsStage = false;
+		int32 IteratorStageId = INDEX_NONE;
+		UFlecsStage* CapturedStage = nullptr;
+
+		flecs::system TestSystem = World()->GetNativeFlecsWorld()
+			.system<>()
+			.kind(flecs::OnUpdate)
+			.run([&bSystemFired, &bIteratorWorldIsStage, &IteratorStageId, &CapturedStage, this](flecs::iter& Iter)
+			{
+				while (Iter.next())
+				{
+					bSystemFired = true;
+					bIteratorWorldIsStage = Iter.world().is_stage();
+					IteratorStageId = Iter.world().get_stage_id();
+					CapturedStage = World()->GetStage(Iter);
+				}
+			});
+
+		ASSERT_THAT(IsTrue(TestSystem.is_valid()));
+
+		TickWorld();
+
+		ASSERT_THAT(IsTrue(bSystemFired));
+		ASSERT_THAT(IsTrue(bIteratorWorldIsStage));
+		ASSERT_THAT(AreEqual(0, IteratorStageId));
+		ASSERT_THAT(IsTrue(CapturedStage != nullptr));
+		ASSERT_THAT(IsTrue(CapturedStage->IsStage()));
+		ASSERT_THAT(AreEqual(0, CapturedStage->GetStageId()));
+		ASSERT_THAT(IsTrue(CapturedStage == World()->GetStage(0)));
+	}
+
 	TEST_METHOD(MultiThreadedSystemGetStage_IterAPI_ReturnsValidStageObject)
 	{
 		if (World()->GetStageCount() < 2)
