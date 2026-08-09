ebe28476a51a2db6dff5f1b320c35094ba944dd7
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2026-08-09 02:34:07 -0400

    D

--- Source/UnrealFlecsGameFramework/Private/Player/FlecsPlayerControllerObjectTag.cpp
+++
@@ -1,7 +0,0 @@
-﻿// Elie Wiese-Namir © 2026. All Rights Reserved.
-
-#include "Player/FlecsPlayerControllerObjectTag.h"
-
-#include UE_INLINE_GENERATED_CPP_BY_NAME(FlecsPlayerControllerObjectTag)
-
-REGISTER_FLECS_COMPONENT(FFlecsPlayerControllerObjectTag);
