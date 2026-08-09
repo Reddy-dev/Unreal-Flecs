a89a165f854dec2390024854aca9ae67b2a7c04a
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2026-08-09 02:34:19 -0400

    Viewer Type

---
+++ Source/UnrealFlecsGameFramework/Private/Viewers/FlecsViewerType.cpp
@@ -0,0 +1,4 @@
+﻿// Elie Wiese-Namir © 2026. All Rights Reserved.
+
+
+#include "Viewers/FlecsViewerType.h"
---
+++ Source/UnrealFlecsGameFramework/Public/Viewers/FlecsViewerType.h
@@ -0,0 +1,24 @@
+﻿// Elie Wiese-Namir © 2026. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Properties/FlecsComponentProperties.h"
+
+#include "FlecsViewerType.generated.h"
+
+UENUM(BlueprintType)
+enum class EFlecsViewerType : uint8
+{
+	Player,
+	Actor,
+	StreamingSource
+}; // enum class EFlecsViewerType
+
+template <>
+struct TFlecsComponentTraits<EFlecsViewerType> : public TFlecsComponentTraitsBase<EFlecsViewerType>
+{
+	static constexpr bool Exclusive = true;
+}; // struct TFlecsComponentTraits<EFlecsViewerType>
+
+
