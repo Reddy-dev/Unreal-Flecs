d24b82d0315bdf6ee91e1bd8bd87a35c2ad2d9ba
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2026-08-27 15:28:33 -0400

    Remove Unused Game Framework Module (will be made in a sep plugin)

--- Source/UnrealFlecsTests/UnrealFlecsTests.Build.cs
+++ Source/UnrealFlecsTests/UnrealFlecsTests.Build.cs
@@ -39,7 +39,6 @@ public class UnrealFlecsTests : ModuleRules
                 "SolidMacros",
                 "FlecsLibrary",
 				"UnrealFlecs",
-				"UnrealFlecsGameFramework",
                 "UnrealFlecsNetworking",
             }
         );
