681e64e47e2e0726984653dede1e996287805dcb
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2026-08-31 16:03:29 -0400

    Update UnrealFlecsTests.cpp

--- Source/UnrealFlecsTests/UnrealFlecsTests.cpp
+++ Source/UnrealFlecsTests/UnrealFlecsTests.cpp
@@ -3,12 +3,15 @@
 #include "UnrealFlecsTests.h"
 
 #include "General/FlecsModuleRegistry.h"
+#include "UnrealFlecsConfigMacros.h"
 
 #define LOCTEXT_NAMESPACE "FUnrealFlecsTestsModule"
 
 void FUnrealFlecsTestsModule::StartupModule()
 {
+#if ENABLE_UNREAL_FLECS_TESTS
 	UE::Flecs::FFlecsModuleRegistry::Get().RegisterUnrealFlecsModule("UnrealFlecsTests");
+#endif // ENABLE_UNREAL_FLECS_TESTS
 }
 
 void FUnrealFlecsTestsModule::ShutdownModule()
